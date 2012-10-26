0b16dc6779b7c0423b3933ffd8a6d5cf3f1590ae
Anthony Zacharakis <anthony@lumoslabs.com>
2012-10-26 14:05:55 -0700

    Remove attribute functionality in prep for model changes

--- lib/rules/parameters/attribute.rb
+++ lib/rules/parameters/attribute.rb
@@ -1,33 +1,17 @@
 module Rules::Parameters
   class Attribute
-    attr_reader :context, :method_chain
+    attr_reader :attribute
 
     def initialize(options = {})
-      @method_chain = options[:attributes] || Array.wrap(options[:attribute])
-      @context      = options[:context]
-      raise 'Attribute parameters must define a context and attributes' unless @context && @method_chain
+      @attribute = options[:attribute]
     end
 
     def evaluate(context = {})
-      raise 'Must specify an attribute to evaluate on the object' if @method_chain.blank?
-      object = context[@context]
-      raise 'Invalid context given' unless object
-      @method_chain.each do |method|
-        raise 'Object does not respond to method' unless object.respond_to?(method.to_sym)
-        object = object.send(method.to_sym)
-      end
-      object
+      context[@attribute]
     end
 
     def cast
       raise NotImplementedError
-    end
-
-    def as_json(options = nil)
-      {
-        context: context,
-        method_chain: method_chain
-      }
     end
   end
 end
