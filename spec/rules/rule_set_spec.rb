b7da43b7a2be040ffc9da1f377362c36f0022898
azach <anthony@lumoslabs.com>
2012-10-26 23:58:09 -0700

    Add some specs for rule set evaluation

--- spec/rules/rule_set_spec.rb
+++ spec/rules/rule_set_spec.rb
@@ -17,6 +17,46 @@ describe Rules::RuleSet do
     end
   end
 
+  describe '#evaluate' do
+    let(:true_rule1) { Rules::Rule.new(lhs_parameter: 'today', evaluator: 'not_nil') }
+    let(:true_rule2) { Rules::Rule.new(lhs_parameter: nil, evaluator: 'nil') }
+    let(:false_rule1) { Rules::Rule.new(lhs_parameter: 'today', evaluator: 'nil') }
+    let(:false_rule2) { Rules::Rule.new(lhs_parameter: nil, evaluator: 'not_nil') }
+
+    context 'when evaluation logic is all' do
+      before { rule_set.evaluation_logic = 'all' }
+
+      it 'returns true if all rules are true' do
+        rule_set.rules << [true_rule1, true_rule2]
+        rule_set.evaluate.should be_true
+      end
+
+      it 'returns false if any rules are false' do
+        rule_set.rules << [true_rule1, false_rule1]
+        rule_set.evaluate.should be_false
+      end
+    end
+
+    context 'when evaluation logic is any' do
+      before { rule_set.evaluation_logic = 'any' }
+
+      it 'returns true if all rules are true' do
+        rule_set.rules << [true_rule1, true_rule2]
+        rule_set.evaluate.should be_true
+      end
+
+      it 'returns true if a single rule is true' do
+        rule_set.rules << [false_rule1, true_rule1]
+        rule_set.evaluate.should be_true
+      end
+
+      it 'returns false if all rules are false' do
+        rule_set.rules << [false_rule1, false_rule2]
+        rule_set.evaluate.should be_false
+      end
+    end
+  end
+
   describe '#attributes' do
     before { Rules::RuleSet.set_attributes_for(FakeClass, attributes) }
 
