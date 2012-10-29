b5101ac347e6236a8c1066fbc6ccaa5dfad70f04
Anthony Zacharakis <anthony@lumoslabs.com>
2012-10-29 11:34:44 -0700

    Revert "Make parameter/evaluator keys into first class DB columns"
    This reverts commit cc9d5df403154816fcf481958c5f0eb9a2f3df6e.

--- spec/dummy/db/migrate/20121029183622_create_rules.rules.rb
+++ spec/dummy/db/migrate/20121029183622_create_rules.rules.rb
@@ -3,10 +3,7 @@ class CreateRules < ActiveRecord::Migration
   def change
     create_table :rules_rules do |t|
       t.belongs_to :rule_set
-      t.string :lhs_parameter_key
-      t.string :rhs_parameter_key
-      t.string :evaluator_key
-      t.text :extra
+      t.text :expression
 
       t.timestamps
     end
