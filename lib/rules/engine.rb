870b63d3aa16b533d1d84f21b9bfe1f710f6c389
Colin Young <me@colinyoung.com>
2020-10-16 12:43:23 -0500

    Style the rules more attractively and ensure the javascript file is added.

--- lib/rules/engine.rb
+++ lib/rules/engine.rb
@@ -2,10 +2,12 @@ module Rules
   class Engine < ::Rails::Engine
     isolate_namespace Rules
 
-    initializer :active_admin do
+    initializer :active_admin do |app|
       if defined? ActiveAdmin
         require 'rules/extensions/active_admin/dsl'
         ActiveAdmin.application.javascripts << 'rules/active_admin.js'
+        app.config.assets.precompile << 'rules/active_admin.css'
+        ActiveAdmin.application.stylesheets['rules/active_admin.css'] = { media: :all }
       end
     end
   end
